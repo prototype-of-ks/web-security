500b06cf5add21e35cf2afc8021d34495a56dd16
prototype-of-ks <2284720956@qq.com>
2022-08-14 18:30:46 +0800

    fix lint issue

--- src/hooks/api/service/index.ts
+++ src/hooks/api/service/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'localhost:8080',
-  timeout: 5 * 60 * 60
+  timeout: 5 * 60 * 60,
 });
 
 export default instance;
--- src/hooks/api/service/usePostCsrf.ts
+++ src/hooks/api/service/usePostCsrf.ts
@@ -4,6 +4,6 @@ import { API_URL } from '../../../constants';
 export default function usePostCsrf() {
   return useRequest({
     method: 'POST',
-    url: API_URL.POST_CSRF
+    url: API_URL.POST_CSRF,
   });
 }
